398053eabdf67eb8bc5babc5104db939b4b84642
Earl Atta-Fynn <earl.atta.fynn@icloud.com>
2025-12-08 19:13:04 -0500

    ssh tty copy paste in nvim_pi

--- .config/nvim_pi/lua/config/options.lua
+++ .config/nvim_pi/lua/config/options.lua
@@ -1,3 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = "unnamedplus"
+
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    -- Paste from the host is often blocked by terminals for security,
+    -- so we rely on the host's native Cmd+V / Ctrl+Shift+V for pasting.
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
